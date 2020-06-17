99a52864797a8254fc832d56d980e1caa6d9d869
scOwez <root@ogriffiths.com>
2020-06-17 23:58:25 +0100

    Closes #8

--- jingo-lib/src/frontend/lexer.rs
+++ jingo-lib/src/frontend/lexer.rs
@@ -5,99 +5,102 @@ use crate::error::JingoError;
 use std::fmt;
 
 /// The token type, represents the type of a [Token] after scanning.
+///
+/// All token types are guaranteed to be displayed using [fmt::Display] in a
+/// human-readable fashion, please reference that if you would like to see what
+/// each type looks like in chars (it is found in `Trait Implementations`).
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub enum TokenType {
-    /// `(`
     LeftBrak,
-    /// `)`
     RightBrack,
-    /// `{`
     LeftBrace,
-    /// `}`
     RightBrace,
-    /// `,`
     Comma,
-    /// `.`
     Dot,
-    /// `+`
     Plus,
-    /// `-`
     Minus,
-    /// `;`
     Semicolon,
-    /// `/`
     FSlash,
-    /// `*`
     Star,
-    /// `!`
     Not,
-    /// `=`
     Equal,
-    /// `<`
     Less,
-    /// `>`
     Greater,
-    /// `!=`
     NotEqual,
-    /// `==`
     EqualEqual,
-    /// `>=`
     GreaterEqual,
-    /// `<=`
     LessEqual,
-    /// `--`, normal eol comment
     Comment,
-    /// `---`, documentation comment, like
-    /// [rusts](https://doc.rust-lang.org/rust-by-example/meta/doc.html#doc-comments).
-    ///
-    /// TODO: do string interning.
-    DocComment(String),
-    /// `-!-`, Similar to a [TokenType::DocComment] but for module headers.
-    ///
-    /// TODO: do string interning.
-    HeaderComment(String),
-    /// Identifier, some non-token set of chars, e.g. `hi` in `fn hi () {}`
-    ///
-    /// TODO: do string interning.
-    Identifier(String),
-    /// A string literal, e.g. `"x"`.
-    StringLit,
-    /// A number literal, e.g. `5`.
-    NumLit,
-    /// `and`
+    DocComment(String),    // TODO: do string interning
+    HeaderComment(String), // TODO: do string interning
+    Identifier(String),    // TODO: do string interning
+    StringLit(String),     // TODO: do string interning
+    NumLit,                // TODO figure out type
     And,
-    /// `class`
     Class,
-    /// `else`
     Else,
-    /// `false`
     False,
-    /// `fn`
     Func,
-    /// `for`
     For,
-    /// `if`
     If,
-    /// `null`
     Null,
-    /// `or`
     Or,
-    /// `print`, **NOTE: may be changed to stdlib func**
-    Print,
-    /// `return`
+    Print, // TODO: change to stdlib func
     Return,
-    /// `super`
     Super,
-    /// `this`
     This,
-    /// `true`
     True,
-    /// `var`
     Var,
-    /// `while`
     While,
-    /// End-of-file
-    Eof,
+}
+
+impl fmt::Display for TokenType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let str_rep = match self {
+            TokenType::LeftBrak => '('.to_string(),
+            TokenType::RightBrack => ')'.to_string(),
+            TokenType::LeftBrace => '{'.to_string(),
+            TokenType::RightBrace => '}'.to_string(),
+            TokenType::Comma => ','.to_string(),
+            TokenType::Dot => '.'.to_string(),
+            TokenType::Plus => '+'.to_string(),
+            TokenType::Minus => '-'.to_string(),
+            TokenType::Semicolon => ';'.to_string(),
+            TokenType::FSlash => '/'.to_string(),
+            TokenType::Star => '*'.to_string(),
+            TokenType::Not => '!'.to_string(),
+            TokenType::Equal => '='.to_string(),
+            TokenType::Less => '<'.to_string(),
+            TokenType::Greater => '>'.to_string(),
+            TokenType::NotEqual => "!=".to_string(),
+            TokenType::EqualEqual => "==".to_string(),
+            TokenType::GreaterEqual => ">=".to_string(),
+            TokenType::LessEqual => "<=".to_string(),
+            TokenType::Comment => "--".to_string(),
+            TokenType::DocComment(content) => format!("--- {}", content),
+            TokenType::HeaderComment(content) => format!("-!- {}", content),
+            TokenType::Identifier(content) => content.clone(),
+            TokenType::StringLit(content) => format!("\"{}\"", content),
+            TokenType::And => "and".to_string(),
+            TokenType::Class => "class".to_string(),
+            TokenType::Else => "else".to_string(),
+            TokenType::Func => "fun".to_string(),
+            TokenType::For => "for".to_string(),
+            TokenType::If => "if".to_string(),
+            TokenType::Null => "null".to_string(),
+            TokenType::Or => "or".to_string(),
+            TokenType::Print => "print".to_string(),
+            TokenType::Return => "return".to_string(),
+            TokenType::Super => "super".to_string(),
+            TokenType::This => "this".to_string(),
+            TokenType::True => "true".to_string(),
+            TokenType::Var => "var".to_string(),
+            TokenType::While => "while".to_string(),
+            _ => "[unknown token]".to_string(), // NOTE: currently numlit
+        };
+
+        write!(f, "{}", str_rep)
+    }
 }
 
 /// Main token representation, combining [TokenType] with metadata.
@@ -106,10 +109,6 @@ pub struct Token {
     /// Type of token an instance of this stuct is referring to.
     pub token_type: TokenType,
 
-    /// The raw character(s) used to make this token (also known as a
-    /// [Lexeme](https://en.wikipedia.org/wiki/Lexeme)).
-    pub raw: String,
-
     /// Line number this token is found on.
     pub line: usize,
 }
@@ -117,18 +116,14 @@ pub struct Token {
 impl Token {
     /// Shortcut for adding a token, taking in the same parameters as normal but
     /// in a more consise manner.
-    pub fn new(token_type: TokenType, raw: String, line: usize) -> Self {
-        Self {
-            token_type,
-            raw,
-            line,
-        }
+    pub fn new(token_type: TokenType, line: usize) -> Self {
+        Self { token_type, line }
     }
 }
 
 impl fmt::Display for Token {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}: `{}`", self.token_type, self.raw)
+        write!(f, "Line {}: {}", self.line, self.token_type)
     }
 }
 
@@ -165,8 +160,8 @@ fn scan_next_token(
         None => return Ok(true),
     };
 
-    let mut add_token = |token_type: TokenType, raw: String| {
-        tokens.push(Token::new(token_type, raw, *cur_line));
+    let mut add_token = |token_type: TokenType| {
+        tokens.push(Token::new(token_type, *cur_line));
     }; // shortcut to add token
 
     let mut peek_next = |next_char: char| -> bool {
@@ -179,59 +174,59 @@ fn scan_next_token(
     }; // peeks for next item and if it exists, consumes it
 
     match c {
-        '(' => add_token(TokenType::LeftBrak, c.to_string()),
-        ')' => add_token(TokenType::RightBrack, c.to_string()),
-        '{' => add_token(TokenType::LeftBrace, c.to_string()),
-        '}' => add_token(TokenType::RightBrace, c.to_string()),
-        ',' => add_token(TokenType::Comma, c.to_string()),
-        '.' => add_token(TokenType::Dot, c.to_string()),
-        ';' => add_token(TokenType::Semicolon, c.to_string()),
-        '/' => add_token(TokenType::FSlash, c.to_string()),
-        '*' => add_token(TokenType::Star, c.to_string()),
-        '+' => add_token(TokenType::Plus, c.to_string()),
+        '(' => add_token(TokenType::LeftBrak),
+        ')' => add_token(TokenType::RightBrack),
+        '{' => add_token(TokenType::LeftBrace),
+        '}' => add_token(TokenType::RightBrace),
+        ',' => add_token(TokenType::Comma),
+        '.' => add_token(TokenType::Dot),
+        ';' => add_token(TokenType::Semicolon),
+        '/' => add_token(TokenType::FSlash),
+        '*' => add_token(TokenType::Star),
+        '+' => add_token(TokenType::Plus),
         '-' => {
             if peek_next('-') {
                 if peek_next('-') {
                     let char_content = get_to_eol(chars);
 
-                    add_token(TokenType::DocComment(char_content), "---".to_string())
+                    add_token(TokenType::DocComment(char_content))
                 } else {
-                    add_token(TokenType::Comment, "--".to_string())
+                    add_token(TokenType::Comment)
                 }
             } else if peek_next('!') && peek_next('-') {
                 let char_content = get_to_eol(chars);
 
-                add_token(TokenType::HeaderComment(char_content), "-!-".to_string())
+                add_token(TokenType::HeaderComment(char_content))
             } else {
-                add_token(TokenType::Minus, c.to_string())
+                add_token(TokenType::Minus)
             }
         } // `-` for minus, `--` for comment, `---` for docstring or `-!-` for header comment
         '=' => {
             if peek_next('=') {
-                add_token(TokenType::EqualEqual, "==".to_string())
+                add_token(TokenType::EqualEqual)
             } else {
-                add_token(TokenType::Equal, c.to_string())
+                add_token(TokenType::Equal)
             }
         } // `=` for equals, `==` for equal to
         '<' => {
             if peek_next('=') {
-                add_token(TokenType::LessEqual, "<=".to_string())
+                add_token(TokenType::LessEqual)
             } else {
-                add_token(TokenType::Less, c.to_string())
+                add_token(TokenType::Less)
             }
         } // `<` for less than, `<=` for less than or equal to
         '>' => {
             if peek_next('=') {
-                add_token(TokenType::GreaterEqual, ">=".to_string())
+                add_token(TokenType::GreaterEqual)
             } else {
-                add_token(TokenType::Greater, c.to_string())
+                add_token(TokenType::Greater)
             }
         } // `>` for greater than, `>=` for greater than or equal to
         '!' => {
             if peek_next('=') {
-                add_token(TokenType::NotEqual, "!=".to_string())
+                add_token(TokenType::NotEqual)
             } else {
-                add_token(TokenType::Not, c.to_string())
+                add_token(TokenType::Not)
             }
         } // `!` for not, `!=` for not equal
         '\n' => *cur_line += 1,  // add line
