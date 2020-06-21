4395371faae5876b9b11b28e6790f6f779fef887
scOwez <root@ogriffiths.com>
2020-06-21 02:12:38 +0100

    Added mods for ast and parser

--- jingo-lib/src/frontend/mod.rs
+++ jingo-lib/src/frontend/mod.rs
@@ -3,4 +3,6 @@
 //! This is the first major stage in this compiler before it's handed over to the
 //! [crate::backend] module for codegen and the final stages.
 
+pub mod ast;
 pub mod lexer;
+pub mod parser;
