12906cb6c8c6addecfd6154679e9a9970155e8e9
Owez <root@ogriffiths.com>
2020-06-21 02:10:36 +0100

    Basic parsing + ast setup

---
+++ jingo-lib/src/frontend/parser.rs
@@ -0,0 +1,3 @@
+//! Parser for Jingo, makes [crate::frontend::lexer] into an AST
+//! (abstract-syntax-tree), the second main stage of a compiler. See
+//! [crate::frontend::ast] for the AST classes.
