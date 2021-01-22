0bb4d212a9a4e10ad28dd333ed6a000c167d7090
Owez <root@ogriffiths.com>
2021-01-22 18:16:17 +0000

    Added binop ast

--- jingo-lib/src/frontend/ast.rs
+++ jingo-lib/src/frontend/ast.rs
@@ -2,4 +2,23 @@
 
 /// Expression enumeration for the AST, the most abstract node type all other
 /// AST items conform to, as everything is considered an expression
-pub enum Expression {}
+pub enum Expression {
+    /// Binary operation allowing two [Expression]s to be modified by a mathmatical
+    /// notation defined in [BinOp]
+    BinOp((Box<Expression>, BinOp, Box<Expression>)),
+}
+
+/// Binary operation enumeration, defining allowed types of an [Expression::BinOp]
+pub enum BinOp {
+    /// Addition
+    Add,
+
+    /// Subtraction
+    Sub,
+
+    /// Multiplication
+    Mul,
+
+    /// Division
+    Div,
+}
